ba059ffb55db66a21ddaa47444dd22ca6876c420
Rogodata <shumov.ai@phystech.edu>
2023-09-27 20:01:01 +0300

    reorganised

--- source/differentiation/TestProg.cpp
+++ source/differentiation/TestProg.cpp
@@ -5,25 +5,30 @@
 #include <fstream>
 #include "differentiation.hpp"
 
-const std::size_t N = 4;
+const std::size_t N = 3;
 
 int main()
 {
-    std::array<double, 4> hCoeff{-1, 1, 2, 3};
-    double h = 10;
+    std::array<double, 3> hCoeff{-1, 1, 2};
+    double h = 1;
     DerivativeCoef<double, N> test_answer = calcDerivativeCoef<double, N>(hCoeff);
-    for (long j = 0; j < 16; j++)
+    for (long j = 0; j < 1e4; j++)
     {
         double diff = 0;
-        h = h / 10;
+        h = h / 1.00369;
         for (auto i = 0; i < N; i++)
         {
             diff += test_answer.otherCoefs[i] * exp(1 + hCoeff[i] * h) / h;
         }
         diff += test_answer.centralCoef * exp(1) / h;
-        std::ofstream fout("/home/rogoda/cpp_projects/CompMath/differentiation/exp_1_4.txt", std::ios::app);
+        /*std::ofstream fout("/home/rogoda/cpp_projects/CompMath/differentiation/exp_1_5.txt", std::ios::app);
         fout << std::fixed << std::setprecision(16) << (diff - exp(1) )/ exp(1) << " " << h << std::endl;
-        fout.close();
+        fout.close();*/
+        for (auto i = 0; i < N; i++)
+        {
+            std::cout << test_answer.otherCoefs[i] << std::endl;
+        }
+        std::cout << test_answer.centralCoef << std::endl;
     }
     return 0;
 }

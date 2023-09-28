d161771ccb5e385aace512f7941a08f2c3a706c9
Rogodata <shumov.ai@phystech.edu>
2023-09-28 21:55:13 +0300

    reorganised more

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rogoda/cpp_projects/CompMath
-# Build directory: /home/rogoda/cpp_projects/CompMath/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("source")

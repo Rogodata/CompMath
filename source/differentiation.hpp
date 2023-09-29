dee275a8bddfc0549e7b73da30a0c229940526c5
Rogodata <shumov.ai@phystech.edu>
2023-09-29 13:00:45 +0300

    more differentiation done

--- source/differentiation.hpp
+++ source/differentiation.hpp
@@ -11,7 +11,49 @@ struct DerivativeCoef
     std::array<RealType, N> otherCoefs;
 };
 
-template <typename RealType, unsigned int N>
+template <typename RealType, unsigned int N, unsigned int L>
+DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N> &points) noexcept
+{
+    // матрица такой слау метода неопределённых коэффициентов необычная, первая строка вся из единиц, а первый столбец,
+    // кроме первого члена стостоит из нулей. Решим слау без первой строки и первого столбца, а центральный коэффициент
+    // рассчитаем отдельно. В отличие от посика первой производной в столбце свободных коэффициентов теперь факториал
+    // порядка производной, чтобы не делать лишние преобразования с матрицей
+    Eigen::Matrix<RealType, Eigen::Dynamic, Eigen::Dynamic> matrix(N, N);
+    Eigen::Vector<RealType, Eigen::Dynamic> coeff(N), eigAns(N);
+    double factorial = 1;
+    for (std::size_t i = 2; i <= L; i++)
+    {
+        factorial *= i;
+    }
+    for (std::size_t i = 1; i < N; i++)
+    {
+        coeff(i) = 0;
+    }
+    coeff(L - 1) = factorial;
+    for (std::size_t i = 0; i < N; i++)
+    {
+        matrix(0, i) = points[i];
+    }
+    for (std::size_t i = 1; i < N; i++)
+    {
+        for (std::size_t j = 0; j < N; j++)
+        {
+            // Нет смысла делить строки матрицы на факториал
+            matrix(i, j) = matrix(i - 1, j) * (points[j]);
+        }
+    }
+    eigAns = matrix.colPivHouseholderQr().solve(coeff);
+    RealType central = -eigAns.sum();
+    std::array<RealType, N> otherArray;
+    for (std::size_t i = 0; i < N; i++)
+    {
+        otherArray[i] = eigAns(i);
+    }
+    return {central, otherArray};
+};
+
+
+/*template <typename RealType, unsigned int N>
 DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N> &points) noexcept
 {
     // points - коэффициенты перед h
@@ -33,7 +75,7 @@ DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N> &po
     {
         for (std::size_t j = 0; j < N; j++)
         {
-            //Нет смысла делить строку матрицы на факториал, ведь в конце концов при решениии слау она должна оказаться нулевой
+            // Нет смысла делить строку матрицы на факториал, ведь в конце концов при решениии слау она должна оказаться нулевой
             matrix(i, j) = matrix(i - 1, j) * (points[j]);
         }
     }
@@ -45,4 +87,4 @@ DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N> &po
         otherArray[i] = eigAns(i);
     }
     return {central, otherArray};
-};
+};*/
--- tests/differentiation_test.cpp
+++ tests/differentiation_test.cpp
@@ -10,7 +10,7 @@ TEST(differentiationtest_test, Test_1)
     std::array<double, 2> hCoeff{-1, 1};
     const double trueCentralCoeff = 0.0;
     const std::array<double, 2> trueOtherCoeff {-0.5, 0.5};
-    DerivativeCoef<double, N> test_answer = calcDerivativeCoef<double, N>(hCoeff);
+    DerivativeCoef<double, N> test_answer = calcDerivativeCoef<double, N, 1>(hCoeff);
     for (size_t i = 0; i < N; ++i)
     {
         ASSERT_NEAR(trueOtherCoeff[i], test_answer.otherCoefs[i], 1e-13);
@@ -24,7 +24,7 @@ TEST(differentiationtest_test, Test_2)
     std::array<double, 2> hCoeff{1, 2};
     const double trueCentralCoeff = -1.5;
     const std::array<double, 2> trueOtherCoeff {2.0, -0.5};
-    DerivativeCoef<double, N> test_answer = calcDerivativeCoef<double, N>(hCoeff);
+    DerivativeCoef<double, N> test_answer = calcDerivativeCoef<double, N, 1>(hCoeff);
     for (size_t i = 0; i < N; ++i)
     {
         ASSERT_NEAR(trueOtherCoeff[i], test_answer.otherCoefs[i], 1e-13);

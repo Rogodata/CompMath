3352734a1ae67fe0b64dfa5ac05a74926fb2e4b7
Rogodata <shumov.ai@phystech.edu>
2023-10-01 23:42:30 +0300

    started interpolation

---
+++ source/interpolation.hpp
@@ -0,0 +1,58 @@
+#include <array>
+
+/**
+ * xType - тип аргумента x.
+ * yType - тип значения функции y
+ * N - количество точек для интерполяции
+ *
+ * Рекомедую обратить внимание. Разность (xType - xType) не обязана быть типом xType
+ */
+template <typename xType, typename yType, unsigned int N>
+class NewtonInterpolator
+{
+private:
+    // точки, последнюю точку хранить не нужно
+    std::array<xType, N - 1> intPoints;
+    // значения разделённых разностей
+    std::array<yType, N> coeffs;
+
+public:
+    NewtonInterpolator(const std::array<xType, N> &points, const std::array<yType, N> &values) noexcept
+    {
+        // конструктор подготовит коэффициенты и далее уже зная их рассчитаем значение
+        for (std::size_t i = 0; i < N - 1; i++)
+        {
+            intPoints[i] = points[i];
+        }
+        coeffs = values;
+        
+        for (std::size_t i = 0; i < N - 1; i++)
+        {
+            for (std::size_t j = N - 1; j > i; j--)
+            {
+                //std::cout << i << " " << j << " " << coeffs[i] << std ::endl;
+                //std::cout << coeffs[j - 1] << " " << j << " " << coeffs[i] << std ::endl;
+                coeffs[j] = (coeffs[j] -  coeffs[j - 1]) / (points[j] - points[j - 1 - i]);
+                
+            }
+        }
+        /*for (auto i = 0; i < N; i++)
+        {
+            std::cout << coeffs[i] << std ::endl;
+        }*/
+        
+        
+    };
+
+    yType interpolate(const xType &x) const noexcept
+    {
+        yType result = coeffs[0];
+        xType mnozh = 1;
+        for (std::size_t i = 0; i < N - 1; i++)
+        {
+            mnozh *= x - intPoints[i];
+            result += mnozh * coeffs[i + 1];
+        }
+        return result;
+    };
+};
--- tests/mainFiles/CompMath.cpp
+++ tests/mainFiles/CompMath.cpp
@@ -1,28 +1,37 @@
 #include <eigen3/Eigen/Dense>
 #include <array>
+#include <iostream>
 #include <iomanip>
 #include <fstream>
-#include "../../source/differentiation.hpp"
+#include "../../source/interpolation.hpp"
 
 const std::size_t N = 5;
 
 int main()
-{
-    std::array<double, 5> hCoeff{-1, 1, 2, 3, 4};
-    double h = 1;
-    DerivativeCoef<double, N> test_answer = calcDerivativeCoef<double, N, 2>(hCoeff);
-    for (long j = 0; j < 1e4; j++)
-    {
-        double diff = 0;
-        h = h / 1.00369;
-        for (auto i = 0; i < N; i++)
+{   
+    double h = 2;
+    double maxErr = 0;
+    double mark = 0;
+    for(int i = 0; i < 1e3; i ++){
+        maxErr = 0;
+        mark = 0;
+        h /= 1.0035;
+        std::array<double, 3> yArr{exp(h /2 - sqrt(3) * h / 4),exp(h / 2),exp(h/2 + sqrt(3) * h / 4)};
+        std::array<double, 3> xArr{h /2 - sqrt(3) * h / 4, h / 2, h/2 + sqrt(3) * h / 4};
+        NewtonInterpolator<double, double, 3> interpolator(xArr, yArr);
+        for (size_t j = 0; j < 1e4; j++)
         {
-            diff += test_answer.otherCoefs[i] * exp(1 + hCoeff[i] * h) / h / h;
+            double err = interpolator.interpolate((h / 1e4) * j) - exp((h / 1e4) * j);
+            if (err > maxErr)
+                maxErr = err;
         }
-        diff += test_answer.centralCoef * exp(1) / h / h;
-        std::ofstream fout("/home/rogoda/cpp_projects/CompMath/tests/mainFiles/differentiation/exp_2_5.txt", std::ios::app);
-        fout << std::fixed << std::setprecision(16) << (diff - exp(1) )/ exp(1) << " " << h << std::endl;
+        std::ofstream fout("/home/rogoda/cpp_projects/CompMath/tests/mainFiles/interpolation/exp_3_cheb.txt", std::ios::app);
+        fout << std::fixed << std::setprecision(16) << maxErr << " " << h << std::endl;
         fout.close();
+              
     }
+    std::cout << h << std::endl;
+    
+    
     return 0;
 }

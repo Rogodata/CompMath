f98ad9f307c4b86f1fc7b4ffe47a251feb4be4b4
Rogodata <shumov.ai@phystech.edu>
2023-10-20 22:24:15 +0300

    added Hermite interpolation

--- source/interpolation.hpp
+++ source/interpolation.hpp
@@ -19,8 +19,8 @@ private:
     std::array<yType, N> coeffs;
 
 public:
-    NewtonInterpolator(const std::array<xType, N> &points, const std::array<yType, N> &values) noexcept 
-    : intPoints{points}, coeffs(values)
+    NewtonInterpolator(const std::array<xType, N> &points, const std::array<yType, N> &values) noexcept
+        : intPoints{points}, coeffs(values)
     {
         // конструктор подготовит коэффициенты и далее уже зная их рассчитаем значение
         for (std::size_t i = 0; i < N - 1; i++)
@@ -35,17 +35,63 @@ public:
     yType interpolate(const xType &x) const noexcept
     {
         // суммируем схемой Горнера
-        /*yType result = coeffs[N - 1] * (x - intPoints[N-2]);
-        for (std::size_t i = N - 2; i > 0; i--)
-        {
-            result = (result + coeffs[i]) * (x - intPoints[i - 1]);
-        }
-        result += coeffs[0];*/
-        // Но вот так выходит точнее, относительная ошибка на рандомных числах порядка 1е-12
         yType result = coeffs[N - 1];
         for (std::size_t i = N - 1; i > 0; i--)
         {
             result = std::fma(result, (x - intPoints[i - 1]), coeffs[i - 1]);
+        }
+        return result;
+    };
+};
+
+/**
+ * xType - тип аргумента x.
+ * yType - тип значения функции y
+ * N - количество точек для интерполяции
+ *
+ * Рекомедую обратить внимание. Разность (xType - xType) не обязана быть типом xType
+ */
+template <typename xType, typename yType, unsigned int N>
+class HermiteInterpolator
+{
+private:
+    // точки
+    std::array<xType, 2 * N> intPoints;
+    // значения разделённых разностей
+    std::array<yType, 2 * N> razn;
+    /*** Какие-то поля ***/
+public:
+    HermiteInterpolator(const std::array<xType, N> &points, const std::array<yType, N> &values, const std::array<yType, N> &deriv) noexcept
+    {
+        // Сделаем хитрый интерполянт Ньютона
+        for (std::size_t i = 0; i < N; i++)
+        {
+            intPoints[2 * i] = points[i];
+            intPoints[2 * i + 1] = points[i];
+        }
+        razn[0] = values[0];
+        razn[1] = deriv[0];
+        for (std::size_t i = 1; i < N; i++)
+        {
+            razn[2 * i] = (values[i] - values[i - 1]) / (points[i] - points[i - 1]);
+            razn[2 * i + 1] = deriv[i];
+        }
+        // сейчас можно воспользоваться схемой из интерполятора Ньютона,где ону итерацию поиска разделённых разностей мы уже провели
+        for (std::size_t i = 1; i < 2 * N - 1; i++)
+        {
+            for (std::size_t j = 2 * N - 1; j > i; j--)
+            {
+                razn[j] = (razn[j] - razn[j - 1]) / (intPoints[j] - intPoints[j - 1 - i]);
+            }
+        }
+    };
+
+    yType interpolate(const xType &x) const noexcept
+    {
+        yType result = razn[2 * N - 1];
+        for (std::size_t i = 2 * N - 1; i > 0; i--)
+        {
+            result = std::fma(result, (x - intPoints[i - 1]), razn[i - 1]);
         }
         return result;
     };
--- tests/mainFiles/CompMath.cpp
+++ tests/mainFiles/CompMath.cpp
@@ -1,40 +1,35 @@
 #include <eigen3/Eigen/Dense>
-#include <vector>
+#include <array>
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 #include <fstream>
-#include "../../source/spline.hpp"
+#include "../../source/interpolation.hpp"
 
 const int N = 6;
 
 int main()
 {
-    double h = 2.5;
+    double h = 2;
     double maxErr = 0;
     double mark = 0;
     for (int i = 0; i < 1e3; i++)
     {
         maxErr = 0;
         mark = 0;
-        h /= 1.0037;
-        std::vector<double> xArr, yArr;
-        xArr.resize(0);
-        yArr.resize(0);
-        for (size_t j = 0; j < 10 / h + 1; j++)
-        {
-            xArr.push_back(j * h);
-            yArr.push_back(std::exp(j*h));
-        }
-        CubicSpline<double, double> Spline(xArr, yArr);
+        h /= 1.0035;
+        std::array<double, N> yArr{exp(0), exp(h / 5), exp(2 * h / 5), exp(3 * h / 5), exp(4 * h / 5), exp(h)};
+        std::array<double, N> xArr{0, h / 5, 2 * h / 5, 3 * h / 5, 4 * h / 5, h};
+
+        HermiteInterpolator<double, double, N> interpolator(xArr, yArr, yArr);
         for (size_t j = 0; j < 1e4; j++)
         {
-            double err = (Spline.interpolate((10. / 1e4) * j) - exp((10. / 1e4) * j)) / exp((10. / 1e4) * j);
+            double err = std::abs(interpolator.interpolate((h / 1e4) * j) - exp((h / 1e4) * j));
             if (err > maxErr)
                 maxErr = err;
         }
-        std::ofstream fout("/home/rogoda/cpp_projects/CompMath/tests/mainFiles/spline/exp_spline_otn.txt", std::ios::app);
-        fout << std::fixed << std::setprecision(16) << maxErr << " " << 10/h + 1 << std::endl;
+        std::ofstream fout("/home/rogoda/cpp_projects/CompMath/tests/mainFiles/interpolation/exp_6_herm.txt", std::ios::app);
+        fout << std::fixed << std::setprecision(16) << maxErr << " " << h << std::endl;
         fout.close();
     }
     std::cout << h << std::endl;
